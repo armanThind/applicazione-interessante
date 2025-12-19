62c8e4d03dea6f10857ab46a1ea0baabb94a85da
armanThind <gpoddighe.mon@gmail.com>
2025-12-19 13:49:11 +0100

    fix: correct header text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React sdfsdfds</h1>
+      <h1>Vite + React </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1n)}>
           count is {count}
